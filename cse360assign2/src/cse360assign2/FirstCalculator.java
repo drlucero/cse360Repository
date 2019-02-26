c4c4471a4bdd0e7d960eb9d184513bb4ff0d1174
David Lucero <draylucero@gmail.com>
2019-02-25 19:51:55 -0700

    Checkin of the First and second versions

---
+++ cse360assign2/src/cse360assign2/FirstCalculator.java
@@ -0,0 +1,75 @@
+/*******************************************************
+* Author: David Lucero
+* Class: CSE 360
+* Class ID: 462
+* Assignment: # 2
+* Date: 2/21/2019
+* Description: This class is a template
+* class that allows for future mathematical solving
+* between integers.  
+********************************************************/
+
+package cse360assign2;
+
+public class FirstCalculator {
+
+	private int total;
+	
+	/**************************************************
+	 * Constructor: Initializes the class variable
+	 * total.
+	 **************************************************/
+	public FirstCalculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**************************************************
+	 * Method getTotal: template for returning total
+	 * of the number.
+	 **************************************************/
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**************************************************
+	 * Method add: Adds integer values.
+	 * @param value
+	 **************************************************/
+	public void add (int value) {
+		
+	}
+	
+	/**************************************************
+	 * Method subtract: Subtracts integer values.
+	 * @param value
+	 **************************************************/
+	public void subtract (int value) {
+		
+	}
+	
+	/**************************************************
+	 * Method multiply: Multiplies integer values.
+	 * @param value
+	 **************************************************/
+	public void multiply (int value) {
+		
+	}
+	
+	/**************************************************
+	 * Method divide: template for dividing integer 
+	 * values.
+	 * @param value
+	 **************************************************/
+	public void divide (int value) {
+		
+	}
+	
+	/**************************************************
+	 * Method getHistory: template for printing past 
+	 * mathematical operations between integers.
+	 * @return String
+	 **************************************************/
+	public String getHistory () {
+		return "";
+	}
+}
